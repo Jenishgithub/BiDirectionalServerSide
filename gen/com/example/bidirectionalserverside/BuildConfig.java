984466f2f2fdcf497e43dbbb6ad38a863cf09883
Jenishgithub <jenish_shakya33@yahoo.com>
2015-08-02 01:02:11 -0800

    Initial commit

---
+++ gen/com/example/bidirectionalserverside/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.bidirectionalserverside;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
